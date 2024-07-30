f8bf0232b36373784b6f05b6e92ca556b0068b26
han-mg <alsrb1277@gmail.com>
2024-07-30 20:01:03 +0900

    check

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     const fetchHello = async () => {
         try {
             const response = await axios.get('http://mingyu.sku-sku.com/api/hello');
+            // 업뎃 확인
             setMessage(response.data);
         } catch (error) {
             console.error('Error fetching the hello message', error);
