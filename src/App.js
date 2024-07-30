399e3e889098f79a89cc1e473f4be672edf4a046
han-mg <alsrb1277@gmail.com>
2024-07-30 18:34:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [message, setMessage] = useState('');
+
+    const fetchHello = async () => {
+        try {
+            const response = await axios.get('http://mingyu.sku-sku.com/hello');
+            setMessage(response.data);
+        } catch (error) {
+            console.error('Error fetching the hello message', error);
+        }
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={fetchHello}>Hello</button>
+                {message && <p>{message}</p>}
+            </header>
+        </div>
+    );
 }
 
 export default App;
